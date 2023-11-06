6dc154ed9245b496fe5a54e24aab0313f0b8033e
xKeyscore <ego.xkeyscore@gmail.com>
2023-11-06 23:29:47 +0800

    UNIT_3

--- src/main/java/UNIT_3/learn/netty/firstExample/TestHttpSeverHandler.java
+++ src/main/java/UNIT_3/learn/netty/firstExample/TestHttpSeverHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /**
  * 自定义的处理器
  */
@@ -20,15 +22,57 @@ public class TestHttpSeverHandler extends SimpleChannelInboundHandler<HttpObject
      */
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        System.out.println(msg.getClass());
+        System.out.println(ctx.channel().remoteAddress());
+        Thread.sleep(8000L);
         if (msg instanceof HttpRequest) {
+            HttpRequest httpReq = (HttpRequest)msg;
+            System.out.println("方法名：" + httpReq.method().name());
+            URI uri = new URI(httpReq.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("请求favicon.ico");
+                return;
+            }
             // 1.构造ByteBuf，内容为 "Hello World"
             ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
             // 2.构造响应
-            FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
+            FullHttpResponse response =
+                    new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, content);
             response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
             response.headers().set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
             // 3.写入并刷新缓冲区
             ctx.writeAndFlush(response);
+            ctx.channel().close();
         }
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel active");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel registered");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("handler added");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel Inactive");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("channel Unregistered");
+        super.channelUnregistered(ctx);
     }
 }
